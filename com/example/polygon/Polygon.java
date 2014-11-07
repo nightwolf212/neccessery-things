42901f8c62301ee3ea775fe566d0da96169cd926
nightwolf212 <nightwolf212@mail.ru>
2014-11-07 21:44:23 +0800

    Critical bug fixed
    Change formulas of calculating new coordinates of vertex

--- com/example/polygon/Polygon.java
+++ com/example/polygon/Polygon.java
@@ -2,15 +2,74 @@ package com.example.polygon;
 
 import java.util.ArrayList;
 
-public class Polygon {
+
+
+public class Polygon{
 	private ArrayList<Point> verts;
+	private float rotCentrX;
+	private float rotCentrY;
+	private float X,Y;
 	public Polygon()
 	{
 		verts=new ArrayList<Point>();
+		X=0;
+		Y=0;
+		rotCentrX=0;
+		rotCentrY=0;
 	}
 	public Polygon(ArrayList<Point> v)
 	{
 		verts=v;
+		rotCentrX=0;
+		rotCentrY=0;
+		X=0;
+		Y=0;
+	}
+	public Polygon(ArrayList<Point> v,float cx, float cy)
+	{
+		verts=v;
+		rotCentrX=cx;
+		rotCentrY=cy;
+	}
+	public Polygon(ArrayList<Point> v,float x, float y,float cx, float cy)
+	{
+		verts=v;
+		rotCentrX=cx;
+		rotCentrY=cy;
+		X=x;
+		Y=y;
+	}
+	public void setRotCentrX(float x)
+	{
+		rotCentrX=x;
+	}
+	public void setRotCentrY(float y)
+	{
+		rotCentrY=y;
+	}
+	public float getRotCentrX()
+	{
+		return rotCentrX;
+	}
+	public float getRotCentrY()
+	{
+		return rotCentrY;
+	}
+	public void setX(float x)
+	{
+		X=x;
+	}
+	public void setY(float y)
+	{
+		Y=y;
+	}
+	public float getX()
+	{
+		return X;
+	}
+	public float getY()
+	{
+		return Y;
 	}
 	public void addPoint(Point t)
 	{
@@ -28,32 +87,49 @@ public class Polygon {
 	{
 		return verts.size();
 	}
-	public ArrayList<Point> getRotatedPoints(float rot, float centrX, float centrY)
+	public ArrayList<Point> getRotatedPoints(float rot)
 	{
 		ArrayList<Point> newPoints=new ArrayList<Point>();
-		Point mid=new Point(centrX,centrY);
+		Point mid=new Point(rotCentrX,rotCentrY);
 		for(int i=0; i<verts.size(); i++)
 		{
 			float dist=getDistBetweenPoints(mid,verts.get(i));
-			float relX=0;
+			
+			if(dist>0.0000001)
+				{float relX=0;
 			relX=verts.get(i).getX()-mid.getX();
 			float relY=0;
 			relY=mid.getY()-verts.get(i).getY();
 			double angl=0;
 			if(relX<0 && relY>0)
-				angl=Math.acos((relY-mid.getY())/dist)*(-1);
+			{
+				float h=(relX);
+				angl=Math.acos((h)/dist);
+			}
 			if(relX<0&&relY<0)
-				angl=Math.acos((relY-mid.getY())/dist)*(-1);
+			{
+				float h=(relX);
+				angl=Math.acos((h)/dist)*(-1);
+			}
 			if(relX>0&&relY<0)
-				angl=Math.acos((relY-mid.getY())/dist);
+			{
+				float h=(relX);
+				angl=Math.acos((h)/dist)*(-1);
+			}
 			if(relX>0 && relY>0)
-				angl=Math.acos((relY-mid.getY())/dist);
-			angl-=rot;
+			{
+				float h=(relX);
+				angl=Math.acos((h)/dist);
+			}
+			angl+=rot;
 			float newX=(float) (dist*Math.cos(angl));
 			float newY=(float) (dist*Math.sin(angl));
 			
 			newPoints.add(new Point(newX,newY));
 			
+				}
+			else
+				newPoints.add(new Point(verts.get(i).getX(),verts.get(i).getY()));
 			
 		}
 		return newPoints;
@@ -62,10 +138,10 @@ public class Polygon {
 	{
 		return (float)Math.sqrt((a.getX()-b.getX())*(a.getX()-b.getX())+(a.getY()-b.getY())*(a.getY()-b.getY()));
 	}
-	public boolean intersectWith(float mRot, float pRot, float mCentrX, float mCentrY,float pCentrX, float pCentrY, Polygon t)
+	public boolean intersectWith(float mRot, Polygon t,float pRot)
 	{
-		ArrayList<Point> p1=this.getRotatedPoints(mRot, mCentrX, mCentrY);
-		ArrayList<Point> p2=t.getRotatedPoints(pRot, pCentrX, pCentrY);
+		ArrayList<Point> p1=this.getRotatedPoints(mRot);
+		ArrayList<Point> p2=t.getRotatedPoints(pRot);
 		p1.add(p1.get(0));
 		p2.add(p2.get(0));
 		for(int i=0; i<p1.size()-1; i++)
